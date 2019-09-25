48d50bd08e71f3055b52cb5a148deb845c30b0d6
Conner Ward <cjw5db@virginia.edu>
2019-09-25 20:24:36 +0700

    add_user utilizes db view

--- models/models/group/group.js
+++ models/models/group/group.js
@@ -1,107 +1,124 @@
 var request = require('request-promise');
 
-var db_url = 'http://couchdb:5984/';
+const db_url = 'http://couchdb:5984/';
+const group_url = db_url + 'groups/';
+const all_views_url = group_url + '_design/all/_view/';
+const user_url = db_url + '_users/org.couchdb.user:'
 
-var group_url = db_url + 'groups/';
+function create(req, res, next){
 
-var user_url = db_url + '_users/org.couchdb.user:'
+  if(!(req.body.name && req.body.owner_id)){
+    return next('missing required data');
+  }
+
+  const name = req.body.name;
+  const owner_id = req.body.owner_id;
+
+  request({
+    url: user_url + owner_id,
+    method: 'HEAD',
+    simple: true,
+  }).then(function() {
+    return request({
+      url: db_url + '_uuids/',
+      method: 'GET',
+      simple: true,
+      json: true,
+    })
+  }).then(function(body){
+    return request({
+      url: group_url + body['uuids'][0],
+      method: 'PUT',
+      body: {
+        "name": name,
+        "owner": owner_id,
+        "users": [],
+      },
+      simple: true,
+      json: true,
+    })
+  }).then(function(body){
+    res.send(body);
+  }).catch(function(err){
+    return next(err);
+  });
+};
+
+//check if owner of group
+//check if new user is valid
+//add user to group
+function add_user(req, res, next){
+
+  if(!(req.body.group_id && req.body.user_id && req.body.owner_id)){
+    return next('missing required data');
+  }
+
+  const group_id = req.body.group_id;
+  const user_id = req.body.user_id;
+  const owner_id = req.body.owner_id;
+
+  request({
+    url: all_views_url + 'user-groups/',
+    method: 'GET',
+    qs: {
+      "key": [
+        owner_id, 
+        0
+      ],
+    },
+    simple: true,
+    json: true,
+  }).then(function(body){
+    if(body.total_rows == 0){
+      return next('user not found');
+    }
+
+    var row = body.rows.find(function(dict){
+      return (dict.id == group_id);
+    });
+
+    if(row == undefined){
+      return next('user is not owner of this group');
+    }
+
+    return request({
+      url: user_url + user_id,
+      method: 'HEAD',
+      simple: true,
+    })
+  }).then(function(){
+    return request({
+      url: group_url + group_id,
+      method: 'GET',
+      simple: true,
+      json: true,
+    })
+  }).then(function(body){
+
+    if(body.users.includes(user_id)){
+      return next('user already in group');
+    }
+
+    body.users.push(user_id);
+    return request({
+      url: group_url + group_id,
+      method: 'PUT',
+      simple: true,
+      json: true,
+      body: body,
+    })
+  }).then(function(body){
+    return res.send(body);
+  }).catch(function(err){
+    return next(err);
+  });
+
+};
 
 module.exports = {
 
-  create: function(req, res, next){
+  create: create,
+  
+  add_user: add_user, 
 
-    if(!req.body.name){
-      return next('missing required data');
-    }
-
-    request({
-      url: group_url,
-      method: 'HEAD',
-      simple: true,
-    }).then(function() {
-      console.log('1');
-      return request({
-        url: db_url + '_uuids/',
-        method: 'GET',
-        simple: true,
-        json: true,
-      })
-    }).then(function(body){
-      console.log('2');
-      return request({
-        url: group_url + body['uuids'][0],
-        method: 'PUT',
-        body: {
-          "name": req.body.name,
-          "users": [],
-        },
-        simple: true,
-        json: true,
-      })
-    }).then(function(body){
-      console.log(2);
-      res.send(body);
-    }).catch(function(err){
-      return next(err);
-    });
-
-  },
-
-  add_user: function(req, res, next){
-
-    if(!(req.body.group_id && req.body.user_id)){
-      return next('missing required data');
-    }
-
-    var user, group;
-
-    request({
-      url: group_url,
-      method: 'HEAD',
-      simple: true,
-    }).then(function() {
-      return request({
-        url: user_url + req.body.user_id,
-        method: 'GET', 
-        simple: true,
-        json: true,
-      })
-    }).then(function(body) {
-      user = body;
-      return request({
-        url: group_url + req.body.group_id,
-        method: 'GET',
-        simple: true,
-        json: true,
-      })
-    }).then(function(body) {
-      group = body;
-      group.users.push(req.body.user_id);
-
-      return request({
-        url: group_url + req.body.group_id,
-        method: 'PUT',
-        simple: true,
-        json: true,
-        body: group,
-      })
-
-    }).then(function() {
-
-      user.groups.push(req.body.group_id);
-
-      return request({
-        url: user_url + req.body.user_id,
-        method: 'PUT',
-        simple: true,
-        json: true,
-        body: user,
-      })
-    }).then(function() {
-      res.status(200).end();
-    }).catch(function(err) {
-      return next(err);
-    });
-    
-  },
 }
--- models/models/user/user.js
+++ models/models/user/user.js
@@ -16,7 +16,6 @@ module.exports = {
         "name": req.body.name, 
         "password": req.body.password, 
         "type": "user",
-        "groups": [],
         "roles": [],
       },
       json: true,
